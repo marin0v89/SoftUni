0c27e6bac56c956c066cfd61ada9b5b26042b711
marin0v89 <xandarous89@gmail.com>
2021-01-20 17:16:15 +0200

    Added third problem

---
+++ Java-Advanced/02.Multidimensional-arrays/Lab/IntersectionOfTwoMatrices_03.java
@@ -0,0 +1,49 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class IntersectionOfTwoMatrices_03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int rows = Integer.parseInt(scan.nextLine());
+        int cols = Integer.parseInt(scan.nextLine());
+
+        char[][] firstMatrix = readMatrix(scan, rows, cols);
+        char[][] secondMatrix = readMatrix(scan, rows, cols);
+
+        char[][] output = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                output[row][col] =
+                        firstMatrix[row][col] == secondMatrix[row][col]
+                                ? firstMatrix[row][col]
+                                : '*';
+            }
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char symbol = output[row][col];
+                System.out.print(symbol+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static char[][] readMatrix(Scanner scan, int rows, int cols) {
+
+        char[][] matrix = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String[] symbols = scan.nextLine().split("\\s+");
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = symbols[col].charAt(0);
+            }
+        }
+
+        return matrix;
+    }
+
+}
