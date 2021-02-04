07c17cb532cbf2354865c0403fc024f721b4e45e
marin0v89 <xandarous89@gmail.com>
2021-02-04 11:05:40 +0200

    Third problem

---
+++ Java-Advanced/05.Defining-Classes/Lab/P03_BankAccound/BankAccount.java
@@ -0,0 +1,31 @@
+package Lab.P03_BankAccound;
+
+public class BankAccount {
+    private static final double DEFAULT_INTEREST_RATE = 0.02;
+    private static double interestRate = DEFAULT_INTEREST_RATE;
+    private static int bankAccountCount = 1;
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = bankAccountCount;
+        bankAccountCount++;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public double getInterest(int years) {
+        return this.balance * interestRate * years;
+    }
+
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+}
---
+++ Java-Advanced/05.Defining-Classes/Lab/P03_BankAccound/Main.java
@@ -0,0 +1,63 @@
+package Lab.P03_BankAccound;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Map<Integer, BankAccount> vault = new HashMap<>();
+
+        String input = scan.nextLine();
+        while (!"End".equals(input)) {
+
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            String output;
+
+            switch (command) {
+                case "Deposit":
+                    int id = Integer.parseInt(tokens[1]);
+                    int amount = Integer.parseInt(tokens[2]);
+                    if (vault.containsKey(id)) {
+                        vault.get(id).deposit(amount);
+                        output = String.format("Deposited %d to ID%d", amount, id);
+                    } else {
+                        output = "Account does not exist";
+                        break;
+                    }
+                    break;
+                case "SetInterest":
+                    double newInterest = Double.parseDouble(tokens[1]);
+                    BankAccount.setInterestRate(newInterest);
+                    output = null;
+                    break;
+                case "GetInterest":
+                    id = Integer.parseInt(tokens[1]);
+                    int years = Integer.parseInt(tokens[2]);
+                    if (vault.containsKey(id)) {
+
+                        double interest = vault.get(id).getInterest(years);
+                        output = String.format("%.2f", interest);
+                    }else {
+                        output = "Account does not exist";
+                        break;
+                    }
+                    break;
+                default:
+                    BankAccount bankAccount = new BankAccount();
+                    vault.put(bankAccount.getId(), bankAccount);
+                    output = "Account ID" + bankAccount.getId() + " created";
+                    break;
+            }
+            if (output != null) {
+                System.out.println(output);
+            }
+
+            input = scan.nextLine();
+        }
+    }
+}
