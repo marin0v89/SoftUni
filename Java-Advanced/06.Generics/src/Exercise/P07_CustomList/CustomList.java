0f99f51ed5ef27378a933819789c7e425e603ecf
marin0v89 <xandarous89@gmail.com>
2021-02-09 23:39:05 +0200

    Added Seventh Problem

---
+++ Java-Advanced/06.Generics/src/Exercise/P07_CustomList/CustomList.java
@@ -0,0 +1,73 @@
+package Exercise.P07_CustomList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList<E extends Comparable<E>> {
+    private List<E> value;
+
+    public CustomList() {
+        this.value = new ArrayList<>();
+    }
+
+    public void add(E item) {
+        this.value.add(item);
+    }
+
+    public E remove(int index) {
+        return this.value.remove(index);
+    }
+
+    public boolean contains(E element) {
+        return this.value.contains(element);
+    }
+
+    public void swap(int indexOne, int indexTwo) {
+        E firstElement = this.value.get(indexOne);
+        this.value.set(indexOne, this.value.get(indexTwo));
+        this.value.set(indexTwo, firstElement);
+    }
+
+    public long countGreater(E element) {
+        return this.value
+                .stream().filter(e -> e.compareTo(element) > 0).count();
+    }
+
+    public E getMax() {
+        E maxEl = null;
+        if (!this.value.isEmpty()) {
+            maxEl = this.value.get(0);
+            for (E e : this.value) {
+                if (e.compareTo(maxEl) > 0) {
+                    maxEl = e;
+                }
+            }
+        }
+        return maxEl;
+    }
+
+    public E getMin() {
+        E maxEl = null;
+        if (!this.value.isEmpty()) {
+            maxEl = this.value.get(0);
+            for (E e : this.value) {
+                if (e.compareTo(maxEl) < 0) {
+                    maxEl = e;
+                }
+            }
+        }
+        return maxEl;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (E e : this.value) {
+            sb
+                    .append(e)
+                    .append(System.lineSeparator());
+        }
+
+        return sb.toString().trim();
+    }
+}
---
+++ Java-Advanced/06.Generics/src/Exercise/P07_CustomList/Main.java
@@ -0,0 +1,51 @@
+package Exercise.P07_CustomList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        CustomList<String> customList = new CustomList<>();
+
+        while (!"END".equals(input)) {
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+            switch (command) {
+                case "Add":
+                    String data = tokens[1];
+                    customList.add(data);
+                    break;
+                case "Remove":
+                    int index = Integer.parseInt(tokens[1]);
+                    customList.remove(index);
+                    break;
+                case "Contains":
+                    data = tokens[1];
+                    System.out.println(customList.contains(data));
+                    break;
+                case "Swap":
+                    int indexOne = Integer.parseInt(tokens[1]);
+                    int indexTwo = Integer.parseInt(tokens[2]);
+                    customList.swap(indexOne, indexTwo);
+                    break;
+                case "Greater":
+                    String element = tokens[1];
+                    System.out.println(customList.countGreater(element));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    System.out.println(customList);
+                    break;
+
+            }
+            input = scan.nextLine();
+        }
+    }
+}
